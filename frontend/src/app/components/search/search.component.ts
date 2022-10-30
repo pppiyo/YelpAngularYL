8e1ed688dcb813370b5f2f19c2d68f4862754cb8
pppiyo <yueqinli@usc.edu>
2022-10-29 18:16:55 -0700

    todo: front end backend communication

--- frontend/src/app/components/search/search.component.ts
+++ frontend/src/app/components/search/search.component.ts
@@ -1,49 +1,100 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder }
-from '@angular/forms';
+   from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';//
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+   selector: 'app-search',
+   templateUrl: './search.component.html',
+   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  userInput: FormGroup;
+   userInput: FormGroup;
+  private httpClient: HttpClient;
 
-  constructor(private fb: FormBuilder) {}
+   constructor(private fb: FormBuilder) { }
 
-  categories = ['Default', 'Arts & Entertainment',
-            'Health & Medical', 'Hotels & Travel',
-            'Food', 'Professional Services'];
+   categories = ['Default', 'Arts & Entertainment',
+      'Health & Medical', 'Hotels & Travel',
+      'Food', 'Professional Services'];
 
-  ngOnInit() {
-    this.userInput = this.fb.group({
-      keyword: ['', Validators.required],
-      distance: ['10', Validators.required],
-      category: [`${this.categories[0]}`, Validators.required],
-      location: ['', Validators.required],
-    });
-  }
+   ngOnInit() {
+      this.userInput = this.fb.group({
+         keyword: ['', Validators.required],
+         distance: ['10', Validators.required],
+         category: [`${this.categories[0]}`, Validators.required],
+         location: ['', Validators.required],
+      });
+   }
 
-    onSubmit(form: FormGroup) {
+   onSubmit(form: FormGroup) {
 
       console.log('Valid?', form.valid); // true or false
       console.log('keyword', form.value.keyword);
       console.log('distance', form.value.distance);
       console.log('category', form.value.category);
+      console.log('location', form.value.location);
       console.log(form.value);
 
+      let params: URLSearchParams = new URLSearchParams();
+      params.set('keyword', form.value.keyword);
+      params.set('distance', form.value.distance);
+      params.set('category', form.value.category);
+      params.set('location', form.value.location);
 
-   }
+      // sent to backend.
 
-  clearAll() {
-    // this.userInput.reset();
-    //todo: more to come
+      this.useIpinfo(this.httpClient);
    }
 
    get form() { return this.userInput.controls; }
 
+  //  useIpinfo(form: FormGroup) {
+  //     fetch("https://ipinfo.io/json?token=f6e03259a7a9e5").then(
+  //        (response) => response.json()
+  //     ).then(
+  //        (jsonResponse) => {
+  //           let coords = jsonResponse.loc;
+  //           let a = coords.split(',');
+  //           var lat = a[0];
+  //           var lng = a[1];
+
+  //           form.value.longitude = lng;
+  //           form.value.latitude = lat;
+
+
+  //           return this.http.get(StaticSettings.BASE_URL, {
+  //              search: jsonFormData
+  //           }).subscribe(
+  //              (response) => this.onGetForecastResult(response.json()),
+  //              (error) => this.onGetForecastError(error.json()),
+  //              () => this.onGetForecastComplete()
+  //           );
+  //        }
+  //     );
+  //  }
+
+
+
+   clearAll() {
+      // this.userInput.reset();
+      //todo: more to come
+   }
+
+  useIpinfo(httpClient: HttpClient) {
+
+
+    this.httpClient.get('https://ipinfo.io/json?token=f6e03259a7a9e5').subscribe(
+       (value: any) => {
+         console.log(value);
+        //  this.userIP = value.ip;
+       },
+       (error) => {
+         console.log(error);
+       }
+     );
+   }
 
 }
 
--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Search } from '../shared/models/Search'
+import { sample_search } from "../../data";
 
 @Injectable({
   providedIn: 'root'
